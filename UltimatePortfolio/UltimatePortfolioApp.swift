d152190157857cf6ea7f30ac5c19c308d0e8918c
Renato Oliveira Fraga <rofraga74@gmail.com>
2024-09-22 15:15:33 -0500

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Renato Oliveira Fraga on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
